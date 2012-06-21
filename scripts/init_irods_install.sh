2e84a3269d2d2ad133492a4f2b94c4f64cfab4cc
Samuel Lampa <samuel.lampa@scilifelab.uu.se>
2012-06-21 14:50:15 +0200

    Added code for creating rescs in init script

--- scripts/init_irods_install.sh
+++ scripts/init_irods_install.sh
@@ -7,7 +7,14 @@
 # Create folders etc
 imkdir -p /ssUppnexZone/proj
 for f in "/" "/ssUppnexZone" "/ssUppnexZone/proj"; do
-  ichmod read public $f;
-done;
+	  ichmod read public $f;
+  done;
 
+  iadmin mkgroup swestoreArchGrp
+
+  iadmin mkresc swestoreArchResc "MSS universal driver" compound kali.uppmax.uu.se /arch2
+  iadmin atrgwestoreArchGrp swestoreArchResc
+
+  iadmin mkresc swestoreArchCacheResc "unix file system" cache kali.uppmax.uu.se /data/rescs/swestoreArchCacheResc
+  iadmin atrgwestoreArchGrp swestoreArchCacheResc
 
